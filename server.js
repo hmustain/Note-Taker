5225e46956d5c56072867d95c0a0e2fc8584c78a
hmustain <hunter@bunkerbranding.com>
2022-12-21 17:38:57 -0600

    updating starter code in server.js

--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 const express = require('express');
 const path = require('path');
 const fs = require('fs');
+const util = require('util');
 
 // create a port and const for app express
 const PORT = 3001;
@@ -24,8 +25,16 @@ app.get('/notes', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/assets/notes.html'))
 ); 
 
+// Promise version of fs.readFile
+const readFromFile = util.promisify(fs.readFile);
+
 // GET Route for retrieving all the notes
 app.get('/api/notes', (req, res) => {
     console.info(`${req.method} request received for notes`);
     readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
   });
+
+//   add code to listen for something on the PORT
+app.listen(PORT, () =>
+  console.log(`App listening at http://localhost:${PORT} 🚀`)
+);
